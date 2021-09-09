8cb18bb3ca146378596f26f6e7551268a7657b29
Adam <azdrojewski099@gmail.com>
2021-09-09 08:40:25 +0200

    Add tvSeriesListViewModel in TvSeriesListActivity

--- app/src/main/java/pl/adam/tv_search/view/activity/TvSeriesListActivity.kt
+++ app/src/main/java/pl/adam/tv_search/view/activity/TvSeriesListActivity.kt
@@ -1,12 +1,16 @@
 package pl.adam.tv_search.view.activity
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import pl.adam.tv_search.R
+import pl.adam.tv_search.viewmodel.TvSeriesListViewModel
 
+@AndroidEntryPoint
 class TvSeriesListActivity : AppCompatActivity() {
 
-
+    private val viewModel: TvSeriesListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
