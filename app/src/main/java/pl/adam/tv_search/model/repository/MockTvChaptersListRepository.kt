9bfbe1495357ff03d31c20a7b534daab4e3db72e
Adam <azdrojewski099@gmail.com>
2021-09-09 10:42:55 +0200

    Update obtainTvSeriesChapterList data in MockTvChaptersListRepository

--- app/src/main/java/pl/adam/tv_search/model/repository/MockTvChaptersListRepository.kt
+++ app/src/main/java/pl/adam/tv_search/model/repository/MockTvChaptersListRepository.kt
@@ -6,17 +6,17 @@ import javax.inject.Inject
 class MockTvChaptersListRepository @Inject constructor() : TvChaptersListRepository {
     override suspend fun obtainTvSeriesChapterList(query: String) = listOf(
         TvChapter(
-            title = "a",
-            img = "b",
-            genre = ""
+            title = "Action",
+            img = "https://static.tvmaze.com/uploads/images/medium_portrait/51/128054.jpg",
+            genre = "Comedy"
         ), TvChapter(
-            title = "a",
-            img = "b",
-            genre = ""
+            title = "Action",
+            img = "https://static.tvmaze.com/uploads/images/medium_portrait/188/470746.jpg",
+            genre = "Sports"
         ), TvChapter(
-            title = "a",
-            img = "b",
-            genre = ""
+            title = "Action Team",
+            img = "https://static.tvmaze.com/uploads/images/medium_portrait/146/367368.jpg",
+            genre = "ITV2"
         )
     )
 }
